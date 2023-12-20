4f0ad591a4088c112a6aecd8d2b2b5fa3ba015f3
yubin <shin.yubin18@gmail.com>
2023-12-20 21:40:32 +0900

    Feat : ApplicationConfig 생성 및 설정
    - Request 마다 로깅 CommonsRequestLoggingFilter
    - CORS 설정

---
+++ src/main/java/com/yubin/chat/config/ApplicationConfig.java
@@ -0,0 +1,27 @@
+package com.yubin.chat.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class ApplicationConfig implements WebMvcConfigurer {
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setIncludeHeaders(true);
+        return loggingFilter;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
