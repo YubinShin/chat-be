cc5dd92f84334e550a89f81f107590ce6a1bb98e
yubin <shin.yubin18@gmail.com>
2024-01-05 17:01:35 +0900

    Feat : Database Seeding 추가
    https://www.baeldung.com/spring-boot-data-sql-and-schema-sql

--- src/main/java/com/yubin/chat/config/SecurityConfiguration.java
+++ src/main/java/com/yubin/chat/config/SecurityConfiguration.java
@@ -1,6 +1,6 @@
 package com.yubin.chat.config;
 
-import com.yubin.chat.user.CustomUserDetailsServiceUserDetailsService;
+import com.yubin.chat.user.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
