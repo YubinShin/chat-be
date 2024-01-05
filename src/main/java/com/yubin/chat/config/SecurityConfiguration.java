4f7d534cf9bb44b77f555fbd6787eadc98595392
yubin <shin.yubin18@gmail.com>
2024-01-05 15:17:56 +0900

    Feat : SecurityConfiguration 에서 WebSecurityConfigurationAdapter 제거

--- src/main/java/com/yubin/chat/config/SecurityConfiguration.java
+++ src/main/java/com/yubin/chat/config/SecurityConfiguration.java
@@ -5,21 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
-import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfiguration {
+public class SecurityConfiguration{
 
     @Autowired
     private AccessDeniedHandler customAccessDeniedHandler;
