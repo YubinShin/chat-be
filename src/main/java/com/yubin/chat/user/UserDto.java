a62203717415a0984597e9aeaad19d765614db57
yubin <shin.yubin18@gmail.com>
2024-01-05 13:57:50 +0900

    Feat : 스프링 시큐리티 UserDto 생성
    일반적으로 이 둘은 별개로 분리하는 것이 좋다.
    
    - Entity : 애플리케이션에서 실제로 사용자를 나타내는 도메인 클래스
    - DTO : 클라이언트에게 데이터를 입력받는 클래스
    
        장점
        1. 보안
        비밀번호나 기타 민감한 정보는 DTO를 통해서는 전달되지 않을 수 있습니다.
        2. 단일책임원칙
        엔티티 구조가 변경되더라도 DTO를 사용함으로써 API 스펙은 그대로 유지될 수 있습니다.

---
+++ src/main/java/com/yubin/chat/user/UserDto.java
@@ -0,0 +1,25 @@
+package com.yubin.chat.user;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+
+    @NotEmpty()
+    private String username;
+
+    @NotEmpty()
+    private String email;
+
+    @NotEmpty()
+    private String password;
+
+    @NotEmpty()
+    private String confirmPassword;
+
+}
